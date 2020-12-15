e53510bd9b4d1950c9719e767f28b0bbd569f18a
21312Julio <21312@student.dorset-college.ie>
2020-12-15 19:35:51 +0000

    commit

---
+++ fewd_lab_1_doc.js
@@ -0,0 +1,39 @@
+//Julio Costa Beyeler 21312 https://github.com/21312Julio/fewd_lab_1.git
+
+//Question 3)
+var firstName = 'Julio';
+//Question 4)
+var favouriteColor = 'Yellow';
+//Question 5)
+var favouriteFood = 'Mashed Potatoes';
+var city = 'Dublin';
+//Question 6) Toe
+var x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+console.log(x);
+//Question 7) Laurel
+var x = 'Laurel';
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+console.log(y);
+//Question 8)
+//This code will print the variable inside brackets.
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood+' '+city);
+//Question 9) Comment above.
+//Question 10)
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+//Question 11)
+console.log(phrase+' '+one+','+two+','+three+','+four+','+five+','+six+','+seven+' '+endingPhrase);
